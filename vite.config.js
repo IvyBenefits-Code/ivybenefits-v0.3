0550e1d7adb59500b06440d62f22cdf442de49f2
Chaitanya Vardhan <chaitanyavardhan1@gmail.com>
2023-06-19 19:01:08 +0530

    add repo in config js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<REPO>/'
+  base: 'https://github.com/IvyBenefits-Code/ivybenefits-v0.3.git'
 })
