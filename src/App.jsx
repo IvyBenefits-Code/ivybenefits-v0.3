a7257bd0a9b4bd6273ca3fc55acf8a2cc9c43300
mridism <mridularya68@gmail.com>
2023-06-19 21:23:12 +0530

    trial2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import use from "react";
+import use3 from "react";
 
 function App() {
     const [count, setCount] = useState(0);
