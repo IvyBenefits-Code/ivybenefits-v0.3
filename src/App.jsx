e171a0159c49725be6193c9418d88f1ec1e00f8d
mridism <mridularya68@gmail.com>
2023-06-19 21:30:45 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,122 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
-import use from "react";
+import video1 from "./demo.mp4";
+import video2 from "./demo2.mp4";
+import video3 from "./demo3.mp4";
+import video4 from "./demo4.mp4";
+import video5 from "./demo5.mp4";
+import logo1 from "./ivy dark.png";
+import logo2 from "./ivy white.png";
+import Hamburger from "hamburger-react";
+
+const videos = [
+    {
+        src: video1,
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        logo: logo1,
+    },
+    {
+        src: video2,
+        text: "dolor sit amet consectetur adipisicing elit.",
+        logo: logo2,
+    },
+    {
+        src: video3,
+        text: "orem ipsum, dolor sit amet consectetur ",
+        logo: logo1,
+    },
+    {
+        src: video4,
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        logo: logo2,
+    },
+    {
+        src: video5,
+        text: ", dolor sit amet consectetur adipisicing elit.",
+        logo: logo1,
+    },
+];
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+    const [showContactScreen, setShowContactScreen] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentVideoIndex(
+                (prevIndex) => (prevIndex + 1) % videos.length
+            );
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const toggleContactScreen = () => {
+        setShowContactScreen(!showContactScreen);
+    };
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
+        <div className="App">
+            <nav>
+                <div className="image-container">
                     <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="Rea logo"
+                        className="fit-content"
+                        src={videos[currentVideoIndex].logo}
+                        alt="Logo"
                     />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-        </>
+                </div>
+                {/* <h2 className="logo">IVY BENEFITS</h2> */}
+                <Hamburger
+                    distance="sm"
+                    size={30}
+                    className="btn"
+                    toggled={showContactScreen}
+                    toggle={toggleContactScreen}
+                ></Hamburger>
+            </nav>
+
+            {showContactScreen ? (
+                <div className="contact-screen">
+                    <h1>Contact Us</h1>
+                    <h3>contact@ivybenefits.com</h3>
+                </div>
+            ) : (
+                <>
+                    <VideoPlayer src={videos[currentVideoIndex].src} />
+
+                    <p>{videos[currentVideoIndex].text}</p>
+
+                    <div className="pagination">
+                        {videos.map((_, index) => (
+                            <button
+                                key={index}
+                                className={`dot ${
+                                    index === currentVideoIndex ? "active" : ""
+                                }`}
+                                onClick={() => setCurrentVideoIndex(index)}
+                            />
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
+    );
+}
+
+function VideoPlayer({ src }) {
+    const ref = useRef();
+
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.src = src;
+            ref.current.load();
+        }
+    }, [src]);
+
+    return (
+        <video className="background-video" ref={ref} autoPlay loop muted>
+            <source src={src} type="video/mp4" />
+        </video>
     );
 }
 
